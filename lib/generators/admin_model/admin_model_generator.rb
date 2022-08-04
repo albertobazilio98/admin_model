b686b0f6feca745c0e7577672757db2301a3e033
Beto <albertobazilio98@gmail.com>
2022-08-04 09:38:57 -0400

    fizing some capitalization and pluralization bugs

--- lib/generators/admin_model/admin_model_generator.rb
+++ lib/generators/admin_model/admin_model_generator.rb
@@ -10,55 +10,29 @@ class AdminModelGenerator < Rails::Generators::NamedBase
   end
 
   def crate_rails_admin_concern
-    @class_name = class_name
+    @pascal_name = class_name.singularize
+    @snake_name = file_name.singularize
     @fields_list = ''
     @attributes.each do |attribute|
       @fields_list << "\n        field :#{attribute.name}"
     end
-    template 'admin_model.template', "app/models/concerns/rails_admin/#{file_name}.rb"
+    template 'admin_model.template', "app/models/concerns/rails_admin/#{@snake_name}.rb"
   end
 
   def include_rails_admin
-    inject_into_class "app/models/#{file_name}.rb", class_name, "  include RailsAdmin::#{class_name}\n"
+    inject_into_class "app/models/#{@snake_name}.rb", @pascal_name, "  include RailsAdmin::#{@pascal_name}\n"
   end
 
   def get_translation_file
-    ptbr = File.read('config/locales/pt-BR.yml')
+    @ptbr_path = 'config/locales/pt-BR.yml'
+    ptbr = File.read(@ptbr_path)
     @ptbr_hash = YAML.load(ptbr)
   end
-
-  def insert_deafult_attributes_translations_if_necessary
-    if @ptbr_hash["pt-BR"]["activerecord"]["attributes"]["default_attributes"].nil?
-      inject_into_file 'config/locales/pt-BR.yml', after: "\n    attributes:\n" do
-        <<-YML
-      default_attributes: &default_attributes
-        id: '#'
-        name: Nome
-        created_at: Data de criação
-        updated_at: Data de atualização
-        YML
-      end
-    end
-  end
-
-  def insert_base_errors_translations_if_necessary
-    if @ptbr_hash["pt-BR"]["activerecord"]["errors"]["models"]["base_errors"].nil?
-      inject_into_file 'config/locales/pt-BR.yml', after: "    errors:\n      models:\n" do
-        <<-YML
-        base_errors: &base_errors
-          blank: não pode ficar em branco
-          invalid: invalido
-          required: é requirido
-          taken: já existe e deve ser único
-        YML
-      end
-    end
-  end
-
   def inssert_model_translation_keys
-    inject_into_file 'config/locales/pt-BR.yml', after: "\n    models:\n" do
+    ptbr = File.read(@ptbr_path)
+    inject_into_file @ptbr_path, after: ptbr[/pt-BR:\n(.*\n)* {2}activerecord:\n(.*\n)* {4}models:\n( {6}.+(\n( {8}.+\n)+))+/] do
       <<-YML
-      #{name}:
+      #{@snake_name}:
         one: please fill me
         other: please fill me
       YML
@@ -69,22 +43,22 @@ class AdminModelGenerator < Rails::Generators::NamedBase
     model_attriutes = @attributes.reduce('') do |acc, attribute|
       "#{acc}\n        #{attribute.name}: please fill me"
     end
-    ptbr = File.read("config/locales/pt-BR.yml")
-    inject_into_file 'config/locales/pt-BR.yml', after: ptbr[/pt-BR:\n(.*\n)* {2}activerecord:\n(.*\n)* {4}attributes:\n( {6}.+(\n( {8}.+\n)+))+/] do
+    ptbr = File.read(@ptbr_path)
+    inject_into_file @ptbr_path, after: ptbr[/pt-BR:\n(.*\n)* {2}activerecord:\n(.*\n)* {4}attributes:\n( {6}.+(\n( {8}.+\n)+))+/] do
       <<-YML
-      #{name}:\n        <<: *default_attributes#{model_attriutes}
+      #{@snake_name}:#{model_attriutes}
       YML
     end
   end
 
   def insert_attributes_errors_translation_keys
     model_attriutes = @attributes.reduce('') do |acc, attribute|
-      "#{acc}\n            #{attribute.name}:\n              <<: *base_errors"
+      "#{acc}\n            #{attribute.name}:"
     end
-    ptbr = File.read("config/locales/pt-BR.yml")
-    inject_into_file 'config/locales/pt-BR.yml', after: ptbr[/pt-BR:\n(.*\n)* {2}activerecord:\n(.*\n)* {4}errors:\n(.*\n)* {6}models:\n( {8}.+(\n( {10}.+\n)+))+/] do
+    ptbr = File.read(@ptbr_path)
+    inject_into_file @ptbr_path, after: ptbr[/pt-BR:\n(.*\n)* {2}activerecord:\n(.*\n)* {4}errors:\n(.*\n)* {6}models:\n( {8}.+(\n( {10}.+\n)+))+/] do
       <<-YML
-        #{name}:\n          attributes:#{model_attriutes}
+        #{@snake_name}:\n          attributes:#{model_attriutes}
       YML
     end
   end
